957ea0ff56caf8f1527f5e597a28f7e55e7db6be
MrBibbles3 <impavidgreen333@gmail.com>
2026-02-15 03:24:06 +1000

    V0.21.1

--- data/cards.js
+++ data/cards.js
@@ -10,7 +10,7 @@ const cards = {
     ,
         {
             id: 'C2',
-            name: 'ERMman',
+            name: 'Crayon Suki',
             price: 100,
             rarity: 'common'            
         }   
@@ -37,6 +37,41 @@ const cards = {
             price: 300,
             rarity: 'epic'            
         }
+    ,
+        {
+            id: 'E2',
+            name: 'GB\'s Dead Girlfriend',
+            price: 300,
+            rarity: 'epic'            
+        }
+    ,
+        {
+            id: 'E3',
+            name: 'LEGO Bibbles',
+            price: 300,
+            rarity: 'epic'            
+        }
+    ,
+        {
+            id: 'E4',
+            name: 'Literally An Apple',
+            price: 300,
+            rarity: 'epic'            
+        }
+    ,
+        {
+            id: 'E5',
+            name: 'Dumb Freddy',
+            price: 300,
+            rarity: 'epic'            
+        }
+    ,
+        {
+            id: 'E6',
+            name: 'SCREEEEEEEEEM',
+            price: 300,
+            rarity: 'epic'            
+        }
     ],
 
     SECRET: [
@@ -46,18 +81,116 @@ const cards = {
             price: 1000,
             rarity: 'secret'            
         }
+    ,
+        {
+            id: 'S2',
+            name: 'Forg',
+            price: 1000,
+            rarity: 'secret'            
+        }
+    ,
+        {
+            id: 'S3',
+            name: 'Cool Bibs',
+            price: 1000,
+            rarity: 'secret'            
+        }
+    ,
+        {
+            id: 'S4',
+            name: 'Angybest',
+            price: 1000,
+            rarity: 'secret'            
+        }
+    ,
+        {
+            id: 'S5',
+            name: 'SaDev',
+            price: 1000,
+            rarity: 'secret'            
+        }
+    ,
+        {
+            id: 'S6',
+            name: 'Kev Is Slep',
+            price: 1000,
+            rarity: 'secret'            
+        }
+    ,
+        {
+            id: 'S7',
+            name: 'Spring-O-Lantern',
+            price: 1000,
+            rarity: 'secret'            
+        }
+    ,
+        {
+            id: 'S8',
+            name: 'Hoopa\'s Revenge',
+            price: 1000,
+            rarity: 'secret'            
+        }
     ],
 
     NIGHTMARE: [
 
         {
             id: 'N1',
-            name: 'Suki The Forg',
+            name: 'Diamond Apple',
             price: 3000,
             rarity: 'nightmare'            
         }
     
         
+    ,
+        {
+            id: 'N2',
+            name: 'Gamebest',
+            price: 3000,
+            rarity: 'nightmare'            
+        }
+    ,
+        {
+            id: 'N3',
+            name: 'Suki',
+            price: 3000,
+            rarity: 'nightmare'            
+        }
+    ,
+        {
+            id: 'N4',
+            name: 'Banjo & Kazooie',
+            price: 3000,
+            rarity: 'nightmare'            
+        }
+    ,
+        {
+            id: 'N5',
+            name: 'Hoopa',
+            price: 3000,
+            rarity: 'nightmare'            
+        }
+    ,
+        {
+            id: 'N6',
+            name: 'Trigger Happy',
+            price: 3000,
+            rarity: 'nightmare'            
+        }
+    ,
+        {
+            id: 'N7',
+            name: 'Legendary Bird Man',
+            price: 3000,
+            rarity: 'nightmare'            
+        }
+    ,
+        {
+            id: 'N8',
+            name: 'SARKK SLOBBER FOFCE',
+            price: 3000,
+            rarity: 'nightmare'            
+        }
     ],
 
     APEX: [
