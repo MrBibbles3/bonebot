99f4c21c8a46f8e5d14f3914e3b9b6aa2ef51e5d
MrBibbles3 <impavidgreen333@gmail.com>
2026-02-14 10:06:13 +1000

    V0.13

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ let currentShopMessages = [];
 let shopEndTime = null;
 let countdownInterval = null;
 let shopHeaderMessage = null;
-const BOT_VERSION = "0.12";
+const BOT_VERSION = "0.13";
 
 
 const client = new Client({
