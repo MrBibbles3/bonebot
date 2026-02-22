e912522d291e8fe5dd5fe7050e5c41dac0aaa3dd
MrBibbles3 <impavidgreen333@gmail.com>
2026-02-22 12:40:57 +1000

    V0.29

--- data/cards.js
+++ data/cards.js
@@ -1,213 +1,469 @@
-const cards = {
-    COMMON: [
-        {
-            id: 'C1',
-            name: 'FakeArin',
-            price: 100,
-            rarity: 'common'            
-        }
-        
+module.exports = {
+  "COMMON": [
+    {
+      "id": "C1",
+      "name": "FakeArin",
+      "price": 100,
+      "rarity": "COMMON"
+    }
     ,
-        {
-            id: 'C2',
-            name: 'Crayon Suki',
-            price: 100,
-            rarity: 'common'            
-        }   
+    {
+      "id": "C2",
+      "name": "Crayon Suki",
+      "price": 100,
+      "rarity": "COMMON"
+    }
     ,
-        {
-            id: 'C3',
-            name: 'GillRunt',
-            price: 69,
-            rarity: 'common'            
-        }
+    {
+      "id": "C3",
+      "name": "GillRunt",
+      "price": 69,
+      "rarity": "COMMON"
+    }
     ,
-        {
-            id: 'C4',
-            name: 'Human Bibbles',
-            price: 100,
-            rarity: 'common'            
-        }
-    ],
-
-    EPIC: [
-        {
-            id: 'E1',
-            name: 'Baby Suki',
-            price: 300,
-            rarity: 'epic'            
-        }
+    {
+      "id": "C4",
+      "name": "Human Bibbles",
+      "price": 100,
+      "rarity": "COMMON"
+    }
     ,
-        {
-            id: 'E2',
-            name: 'GB\'s Dead Girlfriend',
-            price: 300,
-            rarity: 'epic'            
-        }
+    {
+      "id": "C5",
+      "name": "Keyboard Spam",
+      "price": 300,
+      "rarity": "COMMON"
+    }
     ,
-        {
-            id: 'E3',
-            name: 'LEGO Bibbles',
-            price: 300,
-            rarity: 'epic'            
-        }
+    {
+      "id": "C6",
+      "name": "Chair",
+      "price": 300,
+      "rarity": "COMMON"
+    }
     ,
-        {
-            id: 'E4',
-            name: 'Literally An Apple',
-            price: 300,
-            rarity: 'epic'            
-        }
+    {
+      "id": "C7",
+      "name": "Baby Bibble",
+      "price": 300,
+      "rarity": "COMMON"
+    }
     ,
-        {
-            id: 'E5',
-            name: 'Dumb Freddy',
-            price: 300,
-            rarity: 'epic'            
-        }
+    {
+      "id": "C8",
+      "name": "PNG Bibbles",
+      "price": 300,
+      "rarity": "COMMON"
+    }
     ,
-        {
-            id: 'E6',
-            name: 'SCREEEEEEEEEM',
-            price: 300,
-            rarity: 'epic'            
-        }
-    ],
-
-    SECRET: [
-        {
-            id: 'S1',
-            name: 'Dr Toy Freddy',
-            price: 1000,
-            rarity: 'secret'            
-        }
+    {
+      "id": "C9",
+      "name": "Blocky Bibbles",
+      "price": 300,
+      "rarity": "COMMON"
+    }
     ,
-        {
-            id: 'S2',
-            name: 'Forg',
-            price: 1000,
-            rarity: 'secret'            
-        }
+    {
+      "id": "C10",
+      "name": "New Year's Lollie",
+      "price": 300,
+      "rarity": "COMMON"
+    }
     ,
-        {
-            id: 'S3',
-            name: 'Cool Bibs',
-            price: 1000,
-            rarity: 'secret'            
-        }
+    {
+      "id": "C11",
+      "name": "...cat",
+      "price": 300,
+      "rarity": "COMMON"
+    }
     ,
-        {
-            id: 'S4',
-            name: 'Angybest',
-            price: 1000,
-            rarity: 'secret'            
-        }
+    {
+      "id": "C12",
+      "name": "(It's Bibbles)",
+      "price": 300,
+      "rarity": "COMMON"
+    }
     ,
-        {
-            id: 'S5',
-            name: 'SaDev',
-            price: 1000,
-            rarity: 'secret'            
-        }
+    {
+      "id": "C13",
+      "name": "DefinitlyGoat_Boy",
+      "price": 300,
+      "rarity": "COMMON"
+    }
     ,
-        {
-            id: 'S6',
-            name: 'Kev Is Slep',
-            price: 1000,
-            rarity: 'secret'            
-        }
+    {
+      "id": "C14",
+      "name": "O.M.B.",
+      "price": 300,
+      "rarity": "COMMON"
+    }
     ,
-        {
-            id: 'S7',
-            name: 'Spring-O-Lantern',
-            price: 1000,
-            rarity: 'secret'            
-        }
+    {
+      "id": "C15",
+      "name": "Pizzah",
+      "price": 300,
+      "rarity": "COMMON"
+    }
     ,
-        {
-            id: 'S8',
-            name: 'Hoopa\'s Revenge',
-            price: 1000,
-            rarity: 'secret'            
-        }
-    ],
-
-    NIGHTMARE: [
-
-        {
-            id: 'N1',
-            name: 'Diamond Apple',
-            price: 3000,
-            rarity: 'nightmare'            
-        }
-    
-        
+    {
+      "id": "C16",
+      "name": "6400 Raw Salmon",
+      "price": 300,
+      "rarity": "COMMON"
+    }
+  ],
+  "EPIC": [
+    {
+      "id": "E1",
+      "name": "Baby Suki",
+      "price": 300,
+      "rarity": "EPIC"
+    }
     ,
-        {
-            id: 'N2',
-            name: 'Gamebest',
-            price: 3000,
-            rarity: 'nightmare'            
-        }
+    {
+      "id": "E2",
+      "name": "GB's Dead Girlfriend",
+      "price": 300,
+      "rarity": "EPIC"
+    }
     ,
-        {
-            id: 'N3',
-            name: 'Suki',
-            price: 3000,
-            rarity: 'nightmare'            
-        }
+    {
+      "id": "E3",
+      "name": "LEGO Bibbles",
+      "price": 300,
+      "rarity": "EPIC"
+    }
     ,
-        {
-            id: 'N4',
-            name: 'Banjo & Kazooie',
-            price: 3000,
-            rarity: 'nightmare'            
-        }
+    {
+      "id": "E4",
+      "name": "Literally An Apple",
+      "price": 300,
+      "rarity": "EPIC"
+    }
     ,
-        {
-            id: 'N5',
-            name: 'Hoopa',
-            price: 3000,
-            rarity: 'nightmare'            
-        }
+    {
+      "id": "E5",
+      "name": "Dumb Freddy",
+      "price": 300,
+      "rarity": "EPIC"
+    }
     ,
-        {
-            id: 'N6',
-            name: 'Trigger Happy',
-            price: 3000,
-            rarity: 'nightmare'            
-        }
+    {
+      "id": "E6",
+      "name": "SCREEEEEEEEEM",
+      "price": 300,
+      "rarity": "EPIC"
+    }
     ,
-        {
-            id: 'N7',
-            name: 'Legendary Bird Man',
-            price: 3000,
-            rarity: 'nightmare'            
-        }
+    {
+      "id": "E7",
+      "name": "QnA Chair",
+      "price": 500,
+      "rarity": "EPIC"
+    }
     ,
-        {
-            id: 'N8',
-            name: 'SARKK SLOBBER FOFCE',
-            price: 3000,
-            rarity: 'nightmare'            
-        }
-    ],
-
-    APEX: [
-        {
-            id: 'A2',
-            name: 'KevTsuboi',
-            price: 10000,
-            rarity: 'apex'            
-        }
+    {
+      "id": "E8",
+      "name": "Giveaway Boat",
+      "price": 500,
+      "rarity": "EPIC"
+    }
     ,
-        {
-            id: 'A3',
-            name: 'RangaWrath',
-            price: 10000,
-            rarity: 'apex'            
-        }        
-    ]
+    {
+      "id": "E9",
+      "name": "F.N.A.B.",
+      "price": 500,
+      "rarity": "EPIC"
+    }
+    ,
+    {
+      "id": "E10",
+      "name": "One Day",
+      "price": 500,
+      "rarity": "EPIC"
+    }
+    ,
+    {
+      "id": "E11",
+      "name": "Elf Bonnie",
+      "price": 500,
+      "rarity": "EPIC"
+    }
+    ,
+    {
+      "id": "E12",
+      "name": "Rawr",
+      "price": 500,
+      "rarity": "EPIC"
+    }
+    ,
+    {
+      "id": "E13",
+      "name": "Gravelord Bibbles",
+      "price": 500,
+      "rarity": "EPIC"
+    }
+    ,
+    {
+      "id": "E14",
+      "name": "OG Bibbles",
+      "price": 500,
+      "rarity": "EPIC"
+    }
+    ,
+    {
+      "id": "E15",
+      "name": "IRL Suki",
+      "price": 500,
+      "rarity": "EPIC"
+    }
+    ,
+    {
+      "id": "E16",
+      "name": "Earthquake",
+      "price": 500,
+      "rarity": "EPIC"
+    }
+    ,
+    {
+      "id": "E17",
+      "name": "Smiley",
+      "price": 500,
+      "rarity": "EPIC"
+    }
+  ],
+  "SECRET": [
+    {
+      "id": "S1",
+      "name": "Dr Toy Freddy",
+      "price": 1000,
+      "rarity": "SECRET"
+    }
+    ,
+    {
+      "id": "S2",
+      "name": "Forg",
+      "price": 1000,
+      "rarity": "SECRET"
+    }
+    ,
+    {
+      "id": "S3",
+      "name": "Cool Bibs",
+      "price": 1000,
+      "rarity": "SECRET"
+    }
+    ,
+    {
+      "id": "S4",
+      "name": "Angybest",
+      "price": 1000,
+      "rarity": "SECRET"
+    }
+    ,
+    {
+      "id": "S5",
+      "name": "SaDev",
+      "price": 1000,
+      "rarity": "SECRET"
+    }
+    ,
+    {
+      "id": "S6",
+      "name": "Kev Is Slep",
+      "price": 1000,
+      "rarity": "SECRET"
+    }
+    ,
+    {
+      "id": "S7",
+      "name": "Spring-O-Lantern",
+      "price": 1000,
+      "rarity": "SECRET"
+    }
+    ,
+    {
+      "id": "S8",
+      "name": "Hoopa's Revenge",
+      "price": 1000,
+      "rarity": "SECRET"
+    }
+    ,
+    {
+      "id": "S9",
+      "name": "Squeeb",
+      "price": 1000,
+      "rarity": "SECRET"
+    }
+    ,
+    {
+      "id": "S10",
+      "name": "Buff Duo",
+      "price": 1000,
+      "rarity": "SECRET"
+    }
+    ,
+    {
+      "id": "S12",
+      "name": "Endo Bibbles",
+      "price": 1000,
+      "rarity": "SECRET"
+    }
+    ,
+    {
+      "id": "S13",
+      "name": "Get Me a Beer",
+      "price": 1000,
+      "rarity": "SECRET"
+    }
+    ,
+    {
+      "id": "S14",
+      "name": "Impaled Buff Bonnie",
+      "price": 1000,
+      "rarity": "SECRET"
+    }
+    ,
+    {
+      "id": "S15",
+      "name": "Pencil Bibbles",
+      "price": 1000,
+      "rarity": "SECRET"
+    }
+    ,
+    {
+      "id": "S16",
+      "name": "Pencil Suki",
+      "price": 1000,
+      "rarity": "SECRET"
+    }
+    ,
+    {
+      "id": "S17",
+      "name": "Pencil Kev",
+      "price": 1000,
+      "rarity": "SECRET"
+    }
+    ,
+    {
+      "id": "S18",
+      "name": "Spooky Bibbles",
+      "price": 1000,
+      "rarity": "SECRET"
+    }
+    ,
+    {
+      "id": "S19",
+      "name": "Aura Apple",
+      "price": 1000,
+      "rarity": "SECRET"
+    }
+    ,
+    {
+      "id": "S20",
+      "name": "Dev's Mistake",
+      "price": 1000,
+      "rarity": "SECRET"
+    }
+  ],
+  "NIGHTMARE": [
+    {
+      "id": "N1",
+      "name": "Diamond Apple",
+      "price": 3000,
+      "rarity": "NIGHTMARE"
+    }
+    ,
+    {
+      "id": "N2",
+      "name": "Gamebest",
+      "price": 3000,
+      "rarity": "NIGHTMARE"
+    }
+    ,
+    {
+      "id": "N3",
+      "name": "Suki",
+      "price": 3000,
+      "rarity": "NIGHTMARE"
+    }
+    ,
+    {
+      "id": "N4",
+      "name": "Banjo & Kazooie",
+      "price": 3000,
+      "rarity": "NIGHTMARE"
+    }
+    ,
+    {
+      "id": "N5",
+      "name": "Hoopa",
+      "price": 3000,
+      "rarity": "NIGHTMARE"
+    }
+    ,
+    {
+      "id": "N6",
+      "name": "Trigger Happy",
+      "price": 3000,
+      "rarity": "NIGHTMARE"
+    }
+    ,
+    {
+      "id": "N7",
+      "name": "Legendary Bird Man",
+      "price": 3000,
+      "rarity": "NIGHTMARE"
+    }
+    ,
+    {
+      "id": "N8",
+      "name": "SARKK SLOBBER FOFCE",
+      "price": 3000,
+      "rarity": "NIGHTMARE"
+    }
+    ,
+    {
+      "id": "N9",
+      "name": "Burger Special",
+      "price": 3000,
+      "rarity": "NIGHTMARE"
+    }
+    ,
+    {
+      "id": "N10",
+      "name": "$$$$$$$",
+      "price": 3000,
+      "rarity": "NIGHTMARE"
+    }
+    ,
+    {
+      "id": "N11",
+      "name": "Ominous Dev",
+      "price": 3000,
+      "rarity": "NIGHTMARE"
+    }
+    ,
+    {
+      "id": "N12",
+      "name": "Dev Feddy",
+      "price": 3000,
+      "rarity": "NIGHTMARE"
+    }
+  ],
+  "APEX": [
+    {
+      "id": "A2",
+      "name": "KevTsuboi",
+      "price": 10000,
+      "rarity": "APEX"
+    }
+    ,
+    {
+      "id": "A3",
+      "name": "RangaWrath",
+      "price": 10000,
+      "rarity": "APEX"
+    }
+  ]
 };
-
-module.exports = cards;
--- deleteCard.js
+++ deleteCard.js
@@ -16,22 +16,34 @@ async function main() {
 
   const cardId = (await ask('Enter Card ID to delete (e.g. C3): ')).toUpperCase();
 
-  let fileContent = fs.readFileSync(FILE_PATH, 'utf8');
+  // Load actual JS object
+  delete require.cache[require.resolve(FILE_PATH)];
+  const cardsData = require(FILE_PATH);
 
-  const cardRegex = new RegExp(
-    `\\{[^}]*id:\\s*'${cardId}'[^}]*\\},?`,
-    's'
-  );
+  let found = false;
 
-  if (!cardRegex.test(fileContent)) {
+  // Loop through rarities
+  for (const rarity of Object.keys(cardsData)) {
+    const originalLength = cardsData[rarity].length;
+
+    cardsData[rarity] = cardsData[rarity].filter(card => card.id !== cardId);
+
+    if (cardsData[rarity].length !== originalLength) {
+      found = true;
+    }
+  }
+
+  if (!found) {
     console.log('❌ Card not found.');
     rl.close();
     return;
   }
 
-  fileContent = fileContent.replace(cardRegex, '');
+  // Rebuild clean JS file content
+  const newFileContent =
+`module.exports = ${JSON.stringify(cardsData, null, 2)};`;
 
-  fs.writeFileSync(FILE_PATH, fileContent);
+  fs.writeFileSync(FILE_PATH, newFileContent);
 
   console.log(`\n💀 Card ${cardId} deleted successfully.`);
 
